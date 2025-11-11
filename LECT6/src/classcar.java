5283093dbac7576910f38820b9677dcddd7d69dd
rishika patel <https://github.com/rishika26patel-ux/GAME>
2025-11-11 07:28:11 +0530

    java

---
+++ LECT6/src/classcar.java
@@ -0,0 +1,14 @@
+//public class Car {
+  //  String name;
+    //void display(){
+      //  System.out.println("hello");
+    //}
+//}
+//class Car{
+  //  public static void main(String[]args){
+    //    Car obj=new Car();
+      //  obj.display();
+    //}
+//}
+
+
