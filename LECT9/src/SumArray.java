0e3254e13274ccbed9177f548d34441c4b1bb381
rishika patel <https://github.com/rishika26patel-ux/GAME>
2025-11-14 15:55:19 +0530

    java

---
+++ LECT9/src/SumArray.java
@@ -0,0 +1,9 @@
+public class SumArray {
+    public static void main(String[]args)
+    int sum=0;
+    for(int i=0; i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            sum += arr[i][j];
+        }
+    }
+}
