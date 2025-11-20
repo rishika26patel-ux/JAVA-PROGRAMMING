4530c1ae8163d59439ede9075f909bf98caa960d
rishika patel <https://github.com/rishika26patel-ux/GAME>
2025-11-20 13:59:05 +0530

    java

---
+++ LECT12/src/Interface.java
@@ -0,0 +1,22 @@
+public class Interface{
+    public static void main(String[] args)
+    {
+
+       // B b1=new B();
+        //b1.sleep();
+    }
+}
+interface A1
+{
+    void sleep();
+
+
+
+}
+class B1 implements A
+{
+    public void sleep()
+    {
+        System.out.println("sleeppp time!!!!!!!!!!!!!!");
+    }
+}
---
+++ LECT12/src/MultipleInheritance.java
@@ -0,0 +1,17 @@
+interface A{
+    void sleep();
+}
+interface B{
+    void sleep();
+}
+class c implements A,B{
+    public void sleep(){
+        System.out.println("hello");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[]args){
+
+    }
+}
